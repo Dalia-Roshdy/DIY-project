5d658b6711b2606d6e0a85d4a8f8f0484872f452
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-08-13 06:39:07 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -77,16 +77,14 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
       debugLogDiagnostics: true,
       refreshListenable: appStateNotifier,
       navigatorKey: appNavigatorKey,
-      errorBuilder: (context, state) => appStateNotifier.loggedIn
-          ? CLibraryVideoFlowWidget()
-          : AHomePageWidget(),
+      errorBuilder: (context, state) =>
+          appStateNotifier.loggedIn ? AHomePageWidget() : AHomePageWidget(),
       routes: [
         FFRoute(
           name: '_initialize',
           path: '/',
-          builder: (context, _) => appStateNotifier.loggedIn
-              ? CLibraryVideoFlowWidget()
-              : AHomePageWidget(),
+          builder: (context, _) =>
+              appStateNotifier.loggedIn ? AHomePageWidget() : AHomePageWidget(),
         ),
         FFRoute(
           name: AHomePageWidget.routeName,
